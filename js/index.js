4b5a19fcfa1593229d698cdfa41a739d39c157aa
Zovolog <zimoror@gmail.com>
2023-11-18 13:55:35 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function scrollToContact() {
+  var targetBlock = document.getElementById("form-block");
+  targetBlock.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToServices() {
+  var targetBlock = document.getElementById("service-block");
+  targetBlock.scrollIntoView({ behavior: "smooth" });
+}
+
+function openMenu() {
+  const menu = document.querySelector(".mobile-menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
